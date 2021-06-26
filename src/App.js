1c077cf9672becaa4a75135475dfa47b220174dd
Siddhant Gupta <siddhantgupta384@gmail.com>
2021-06-26 21:28:21 +0530

    navbar-done
    ...

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react"; 
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Header from "./Header";
+
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+  }
+  
+
+export default App;
