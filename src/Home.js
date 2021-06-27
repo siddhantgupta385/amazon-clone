fe05f377196ba5dd62170c713efd540620e63a29
Siddhant Gupta <siddhantgupta384@gmail.com>
2021-06-27 13:13:40 +0530

    product and payments

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ function Home() {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
         />
+
         <div className="home__row">
           <Product
             id="12321341"
@@ -64,6 +65,6 @@ function Home() {
       </div>
     </div>
   );
-};
+}
 
 export default Home;
