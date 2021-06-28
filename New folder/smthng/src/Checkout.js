5a5ebf0d59a59ba832f11c7f39088d7fb28584e6
Siddhant Gupta <siddhantgupta384@gmail.com>
2021-06-28 14:39:35 +0530

    checkout & subtotal updated

--- New folder/smthng/src/Checkout.js
+++ New folder/smthng/src/Checkout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Checkout.css";
-// import Subtotal from "./Subtotal";
+import Subtotal from "./Subtotal";
 // import { useStateValue } from "./StateProvider";
 // import CheckoutProduct from "./CheckoutProduct";
 
@@ -16,11 +16,11 @@ function Checkout() {
           alt=""
         />
 
-        {/* <div>
-          <h3>Hello, {user?.email}</h3>
-          <h2 className="checkout__title">Your shopping Basket</h2>
+        <div>
+          {/* <h3>Hello, {user?.email}</h3> */}
+          // <h2 className="checkout__title">Your shopping Basket</h2>
 
-          {basket.map(item => (
+          {/* {basket.map(item => (
             <CheckoutProduct
               id={item.id}
               title={item.title}
@@ -28,13 +28,13 @@ function Checkout() {
               price={item.price}
               rating={item.rating}
             />
-          ))}
+          ))} */}
 
-        </div> */}
+        </div>
       </div>
 
       <div className="checkout__right">
-        {/* <Subtotal /> */}
+        <Subtotal />
       </div>
     </div>
   );
--- New folder/smthng/src/Subtotal.js
+++ New folder/smthng/src/Subtotal.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
-import { useStateValue } from "./StateProvider";
-import { getBasketTotal } from "./reducer";
-import { useHistory } from "react-router-dom";
+// import { useStateValue } from "./StateProvider";
+// import { getBasketTotal } from "./reducer";
+// import { useHistory } from "react-router-dom";
 
 function Subtotal() {
-  const history = useHistory();
-  const [{ basket }, dispatch] = useStateValue();
+  // const history = useHistory();
+  // const [{ basket }, dispatch] = useStateValue();
 
   return (
     <div className="subtotal">
@@ -15,8 +15,7 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              {/* Part of the homework */}
-              Subtotal ({basket.length} items): <strong>{value}</strong>
+              Subtotal (0 items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -24,13 +23,14 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={getBasketTotal(basket)} // Part of the homework
+        value={0} 
         displayType={"text"}
         thousandSeparator={true}
-        prefix={"$"}
+        prefix={"₹"}
       />
 
-      <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
+      <button >Proceed to Checkout</button>
+      {/* <button onClick={e => history.push('/payment')}>Proceed to Checkout</button> */}
     </div>
   );
 }
