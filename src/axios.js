a88fa8e4d6fced6992ab250b1ffa27ca5fed255a
Siddhant Gupta <siddhantgupta384@gmail.com>
2021-07-01 20:39:13 +0530

    ....

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'http://localhost:5001/clone-a64a4/us-central1/api'
+  baseURL: 'https://us-central1-clone-a64a4.cloudfunctions.net/api'
+  // baseURL: 'http://localhost:5001/clone-a64a4/us-central1/api'
 });
 
 export default instance;
